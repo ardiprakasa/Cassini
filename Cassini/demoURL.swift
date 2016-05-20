feb5e4931b983445ee743f8037ec01cd443893ca
Hardian Prakasa <hardian@android-572eceafe73b2668.icehousecorp.local>
2016-05-20 19:13:16 +0700

    implementing scrollview and multithreading

---
+++ Cassini/demoURL.swift
@@ -0,0 +1,20 @@
+//
+//  demoURl.swift
+//  Cassini
+//
+//  Created by Hardian Prakasa on 5/20/16.
+//  Copyright © 2016 Ice House Corp. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let Stanford = NSURL(string: "http://comm.stanford.edu/wp-content/uploads/2013/01/stanford-campus.png")
+    
+    struct NASA {
+        static let Cassini = NSURL(string: "http://habrastorage.org/getpro/geektimes/post_images/d4e/d2f/9f1/d4ed2f9f1bf5d4f924c1ef19c31366e8.png")
+        static let Earth = NSURL(string: "http://solarviews.com/raw/earth/bluemarblewest.jpg")
+        static let Saturn = NSURL(string: "http://nssdc.gsfc.nasa.gov/planetary/image/saturn.jpg")
+    }
+    
+}
